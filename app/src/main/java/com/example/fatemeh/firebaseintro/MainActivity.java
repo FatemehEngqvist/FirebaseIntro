914acc6484bc9fbe6bb9c7fdfc6a7c28e155bd81
Fatemeh Engqvist <fatemeh.engqvist@gmail.com>
2018-12-12 14:07:37 +0100

    Add and retrieve data from firebase

--- app/src/main/java/com/example/fatemeh/firebaseintro/MainActivity.java
+++ app/src/main/java/com/example/fatemeh/firebaseintro/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.fatemeh.firebaseintro;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+        myRef.setValue("Another Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
